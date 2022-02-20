50b9f5e2502cb322c154f8a1c43f8161d031f588
Thukyd <jptebbe@hotmail.com>
2022-02-20 20:47:46 +0100

    solved

---
+++ coding_challenges/spiralize_matrix.ts
@@ -0,0 +1,43 @@
+/*
+unitalife
+Spiralize Matrix
+You are attempting to solve a Coding Contract. You have 10 tries remaining, after which the contract will self-destruct.
+
+
+Given the following array of arrays of numbers representing a 2D matrix, return the elements of the matrix as an array in spiral order:
+
+    [
+        [27,20, 2,20,47,18,24,42,35]
+        [29,26, 7,31,41,22,17,46,12]
+        [21,14,11,48,45,20,23,49, 9]
+        [10,16,41,33,49,19,11,44,37]
+        [50, 7,28,37, 9,42, 1,18,26]
+        [47,43,39,11,43,47,16, 8,25]
+        [40,42,32,35,24,43,24,35,44]
+        [46,44,42,21,50,50,13,49,18]
+        [26,50,23,17,27, 3,19,31, 9]
+        [29,47,37, 5,40,29,23, 5,12]
+        [30, 3, 3,41,40,43,37,31,19]
+        [38,38, 9,50, 3, 6,12,14,29]
+    ]
+
+Here is an example of what spiral order should be:
+
+    [
+        [1, 2, 3]
+        [4, 5, 6]
+        [7, 8, 9]
+    ]
+
+Answer: [1, 2, 3, 6, 9, 8 ,7, 4, 5]
+
+Note that the matrix will not always be square:
+
+    [
+        [1,  2,  3,  4]
+        [5,  6,  7,  8]
+        [9, 10, 11, 12]
+    ]
+
+Answer: [1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7]
+*/
