d591fabc3e881842763a8521b9423d7fa54312fe
Thukyd <>
2022-02-20 07:27:00 +0100

    Algorithmic Stock Trade II solution & some code clean up

---
+++ coding_challenges/algorithmic_stock_trader_i.ts
@@ -0,0 +1,40 @@
+/*
+CSEC
+Algorithmic Stock Trader I
+You are attempting to solve a Coding Contract. You have 5 tries remaining, after which the contract will self-destruct.
+
+
+You are given the following array of stock prices (which are numbers) where the i-th element represents the stock price on day i:
+
+43,65,133,117,15,102,17,187,20,84,57,129,123,150,69,108,183,186,75,191
+
+Determine the maximum possible profit you can earn using at most one transaction (i.e. you can only buy and sell the stock once). If no profit can be made then the answer should be 0. Note that you have to buy the stock before you can sell it
+*/
+
+let stockPricesI = [43,65,133,117,15,102,17,187,20,84,57,129,123,150,69,108,183,186,75,191];
+
+// find the maximum incline => 15 - 191
+
+function algoTraderI (prices:number[]) {
+    let maxProfit = 0;
+
+    // sort first
+    let sortedPrices =  prices.sort((a, b) => (a > b) ? 1 : -1);
+    // create Array to manipulate
+    let checkArr = [...sortedPrices]; 
+    // check loop from min to max
+    for (let i = 0; i < sortedPrices.length; i++) {
+        let min = checkArr[i]
+        let max = checkArr[checkArr.length - 1]
+
+        if (prices.indexOf(min) < prices.indexOf(max)){
+            maxProfit = max - min;
+            break;
+        }
+    }
+    return maxProfit
+}
+
+
+
+console.log(algoTraderI(stockPricesI))
--- coding_challenges/algorithmic_stock_trader_ii.ts
+++ coding_challenges/algorithmic_stock_trader_ii.ts
@@ -13,3 +13,36 @@ Determine the maximum possible profit you can earn using as many transactions as
 If no profit can be made, then the answer should be 0
 
 */
+
+let stockPricesII = [159,3,172,44,161,77,80,76,50,108,81,43,114,154,34,191,97,140,53,4,33,151,98,181];
+
+// find the maximum incline => 15 - 191
+
+// FIXME still version of before
+    // now you can do as many transactions as you like
+    // you can't sell before buying
+// TODO:
+    // check for each increasing trend
+    // add the distance as max profit
+function algoTraderII (prices:number[]) {
+    let maxProfit = 0;
+
+    for(let i = 1; i < prices.length; i++){
+        if (prices[i] > prices[i-1]) {
+            //console.log(`Kaufen von ${prices[i-1]} bis ${prices[i]}`)
+            maxProfit += prices[i] - prices[i-1]
+        }
+    }
+    return maxProfit
+}
+
+
+
+console.log(`The max profit is : ${algoTraderII(stockPricesII)}`)
+/** Expected buying periods
+ * 3 => 172
+ * 44 => 161
+ * 77 => 80
+ * 50 => 108
+ * ....
+ */
