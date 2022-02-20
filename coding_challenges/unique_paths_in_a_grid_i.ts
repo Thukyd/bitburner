1e75dc68033321d16e2c3f3ef5d8bdbc3424a0be
Thukyd <jptebbe@hotmail.com>
2022-02-20 20:55:44 +0100

    add new challenge

---
+++ coding_challenges/unique_paths_in_a_grid_i.ts
@@ -0,0 +1,12 @@
+/*
+neo-net
+Unique Paths in a Grid I
+You are attempting to solve a Coding Contract. You have 10 tries remaining, after which the contract will self-destruct.
+
+
+You are in a grid with 10 rows and 2 columns, and you are positioned in the top-left corner of that grid. You are trying to reach the bottom-right corner of the grid, but you can only move down or right on each step. Determine how many unique paths there are from start to finish.
+
+NOTE: The data returned for this contract is an array with the number of rows and columns:
+
+[10, 2]
+ */
