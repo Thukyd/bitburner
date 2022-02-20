ae2e1753257e2c87d5c38a8ca615a7be639ddda8
Thukyd <jptebbe@hotmail.com>
2022-02-20 19:05:32 +0100

    added new challenge

---
+++ coding_challenges/array_jumping_game.ts
@@ -0,0 +1,15 @@
+/*
+zb-institute
+Array Jumping Game
+You are attempting to solve a Coding Contract. You have 1 tries remaining, after which the contract will self-destruct.
+
+
+You are given the following array of integers:
+
+1,0,6
+
+Each element in the array represents your MAXIMUM jump length at that position. This means that if you are at position i and your maximum jump length is n, you can jump to any position from i to i+n.
+
+Assuming you are initially positioned at the start of the array, determine whether you are able to reach the last index.
+*/
+
